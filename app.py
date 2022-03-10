64cf8a78458973f7ef5829499e5a2c80825d3290
gooodinho <andrey09132000@gmail.com>
2022-03-10 15:41:48 +0200

    template setup

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import handlers
+from util.misc import logging
 from aiogram import executor
 from loader import dp
 
 
 async def on_startup(dispatcher):
-    print('Bot has been launched')
+    pass
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
