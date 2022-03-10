6b6279d8349ed20741435e3e940347fc1eabcf4c
gooodinho <andrey09132000@gmail.com>
2022-03-10 18:27:02 +0200

    add postgresql settings + connection

---
+++ util/db_api/postgresql.py
@@ -0,0 +1,68 @@
+from typing import Union
+import asyncpg
+from asyncpg import Connection
+from asyncpg.pool import Pool
+
+from data import config
+
+
+class Database:
+
+    def __init__(self):
+        self.pool: Union[Pool, None] = None
+
+    async def create(self):
+        self.pool = await asyncpg.create_pool(
+            user=config.DB_USER,
+            password=config.DB_PASS,
+            host=config.DB_HOST,
+            database=config.DB_NAME
+        )
+
+    async def execute(self, command, *args,
+                      fetch: bool = False,
+                      fetch_val: bool = False,
+                      fetch_row: bool = False,
+                      execute: bool = False
+                      ):
+        async with self.pool.acquire() as connection:
+            connection: Connection
+            async with connection.transaction():
+                if fetch:
+                    result = await connection.fetch(command, *args)
+                elif fetch_val:
+                    result = await connection.fetchval(command, *args)
+                elif fetch_row:
+                    result = await connection.fetchrow(command, *args)
+                elif execute:
+                    result = await connection.execute(command, *args)
+            return result
+
+    async def create_table_admins(self):
+        sql = """
+        CREATE TABLE IF NOT EXISTS Admins (
+        id SERIAL PRIMARY KEY,
+        full_name VARCHAR(255) NOT NULL,
+        username VARCHAR(255) NULL,
+        telegram_id BIGINT NOT NULL
+        );        
+        """
+
+        await self.execute(sql, execute=True)
+
+    @staticmethod
+    def format_args(sql, parameters: dict):
+        sql += " AND ".join([
+            f"{item} = ${num}" for num, item in enumerate(parameters.keys(),
+                                                          start=1)
+        ])
+        return sql, tuple(parameters.values())
+
+    async def select_all_admins(self):
+        sql = "SELECT * FROM Admins"
+        await self.execute(sql, fetch=True)
+
+    async def select_admin(self, **kwargs):
+        sql = "SELECT * FROM Admins WHERE "
+        sql, parameters = self.format_args(sql, parameters=kwargs)
+        return await self.execute(sql, *parameters, fetch_row=True)
