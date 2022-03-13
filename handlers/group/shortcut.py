78b2e7877ea7ef3d6e714a9bd70fea90420a629e
gooodinho <andrey09132000@gmail.com>
2022-03-13 19:37:02 +0200

    replace short to full text in group

--- handlers/group/shortcut.py
+++ handlers/group/shortcut.py
@@ -6,4 +6,6 @@ from aiogram import types
 @dp.message_handler(IsGroup(), AdminFilter(), regexp='^!\S*$')
 async def change_shortcut(message: types.Message):
     await message.delete()
-    await message.answer("*")
+    short = message.text[1:]
+    shortcut = await db.select_shortcut(short=short)
+    await message.answer(shortcut.get('full_text'))
