0f877fcf95897619d2b6e7939654560a66888da9
gooodinho <andrey09132000@gmail.com>
2022-03-17 18:51:49 +0200

    fix

--- data/config.py
+++ data/config.py
@@ -16,4 +16,6 @@ DB_PASS = env.str("DB_PASS")
 DB_NAME = env.str("DB_NAME")
 DB_HOST = env.str("DB_HOST")
 
+REDIS_HOST = env.str("REDIS_HOST")
+
 POSTGRES_URI = f"postgresql://{DB_USER}:{DB_PASS}@{IP}/{DB_NAME}"
--- loader.py
+++ loader.py
@@ -6,7 +6,7 @@ from data import config
 from util.db_api.postgresql import Database
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = RedisStorage2(host=config.DB_HOST)
+storage = RedisStorage2(host=config.REDIS_HOST)
 dp = Dispatcher(bot, storage=storage)
 db = Database()
 
