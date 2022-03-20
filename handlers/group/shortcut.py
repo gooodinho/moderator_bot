1c68556abbf4488a20a5fd3531f2807990a14450
gooodinho <andrey09132000@gmail.com>
2022-03-20 15:30:06 +0200

    fix

--- handlers/group/shortcut.py
+++ handlers/group/shortcut.py
@@ -5,7 +5,8 @@ from aiogram import types
 
 @dp.message_handler(IsGroup(), AdminFilter(), regexp='^!\S*$')
 async def change_shortcut(message: types.Message):
-    await message.delete()
     short = message.text[1:]
     shortcut = await db.select_shortcut(short=short)
-    await message.answer(shortcut.get('full_text'))
+    if shortcut:
+        await message.delete()
+        await message.answer(shortcut.get('full_text'))
